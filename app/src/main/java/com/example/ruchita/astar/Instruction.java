78321bfbb50337a3d9ea996487bc29d8fc43da96
shahruchita20 <shahruchita20@gmail.com>
2016-09-29 23:00:30 +0530

    Added Instruction activity

---
+++ app/src/main/java/com/example/ruchita/astar/Instruction.java
@@ -0,0 +1,15 @@
+package com.example.ruchita.astar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Instruction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instruction);
+
+
+    }
+}
